562d7b903689f419d9a406fc14381f675d32de20
kirtan2004khatri <kirtan2004khatri@gmail.com>
2022-07-04 14:12:04 +0530

    pdf functionality done

--- src/App.vue
+++ src/App.vue
@@ -3,8 +3,8 @@
     <v-app-bar absolute temporary app dense class="green white--text">
       <v-toolbar-title class="font-weight-bold text-h5">Tabler</v-toolbar-title>
       <v-spacer></v-spacer>
-      <v-btn flat text class="white--text" router to="/">Form</v-btn>
-      <v-btn flat text class="white--text" router to="/about">Table</v-btn>
+      <v-btn  text class="white--text" router to="/">Form</v-btn>
+      <v-btn text class="white--text" router to="/about">Table</v-btn>
     </v-app-bar>
     <v-main>
       <router-view></router-view>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -7,79 +7,100 @@
         Pdf
       </v-btn>
     </v-container>
-  <v-container>
-    <vue-good-table 
-    :columns="head"
-    :rows="rows"
-    theme="nocturnal"
-  />
-  </v-container>
+    <v-container>
+      <vue-good-table :columns="head" :rows="rows" theme="nocturnal" :search-options="{
+        enabled: true,
+        trigger: 'enter',
+        skipDiacritics: true,
+        placeholder: 'Search any data here....'
+      }" :sort-option="{
+  enabled: true
+}" />
+    </v-container>
   </div>
 </template>
 
 <script>
 import 'vue-good-table/dist/vue-good-table.css'
-import {VueGoodTable} from 'vue-good-table'
+import { VueGoodTable } from 'vue-good-table'
 import jsPdf from 'jspdf'
 import autoTable from 'jspdf-autotable'
-import {colRef} from '../../src/firebase-config'
-import {onSnapshot} from '@firebase/firestore'
-  export default {
-    name: 'Home',
-    data(){
-      return{
-        head:[
-          {label:'Name',field:'name'},
-          {label:'Age',field:'age',type:'number'},
-          {label:'Coding Lang',field:'coding'},
-          {label:'Expereince',field:'exp'}
-        ],
-        rows:[]
-      }
-    },
-    components: {
-      VueGoodTable
-    },
-    methods:{
-      fetchData(){
-        let data=[]
-        onSnapshot(colRef,snapshot=>{
-          snapshot.docs.forEach(e=>{
-            data.push({...e.data(),id:e.id})
-          });
-          this.rows=data;
-          console.log(data);
-          data=[];
-        });
-      },
-      download(){
-        let arr=[],header=[];
-        this.rows.forEach((a,b,c,d,)=>{
-          // arr.push((Object.values(e)).sort());
-          console.log(a,b,c,d);
-        })
-        // This is for the header
-        header.push([])
-        this.head.forEach(e=>{
-          header[0].push(e.label)
-        })
-        console.log(arr)
-        let doc=new jsPdf();
-        autoTable(doc,{
-          head:header,
-          body:arr,
-          // didParseCell:function(HookPage){
-          //   if(HookPage.cell.text=='Python'){
-          //     HookPage.cell.styles.fillColor="yellow"
-          //   }
-          //   console.log(HookPage);
-          // }
-        });
-        // doc.save('yeah.pdf')
-      }
-    },
-    created(){
-      this.fetchData();
+import { colRef } from '../../src/firebase-config'
+import { onSnapshot } from '@firebase/firestore'
+import { map } from '@firebase/util'
+export default {
+  name: 'Home',
+  data() {
+    return {
+      head: [
+        { label: 'Name', field: 'name' },
+        { label: 'Age', field: 'age', type: 'number' },
+        { label: 'Coding Lang', field: 'coding' },
+        { label: 'Expereince', field: 'exp' }
+      ],
+      rows: []
     }
+  },
+  components: {
+    VueGoodTable
+  },
+  methods: {
+    fetchData() {
+      let data = [], arr = [];
+      onSnapshot(colRef, snapshot => {
+        snapshot.docs.forEach(e => {
+          data.push({ ...e.data(), id: e.id })
+        });
+        this.rows = data;
+        console.log(data);
+        data = [];
+      });
+    },
+    download() {
+      let arr = [], header = [];
+      // This is for the header
+      header.push([])
+      this.head.forEach(e => {
+        header[0].push(e.field)
+      })
+      let doc = new jsPdf();
+
+    //logic for sorting an array in correct way form object 
+      this.rows.forEach((e,index)=> {
+        arr.push([])
+        for (let i of header[0]) {
+          arr[index].push(e[i]);
+        }
+      });
+      autoTable(doc, {
+        head: header,
+        body: arr,
+        didParseCell:function(HookPage){
+          if(HookPage.cell.text=='JavaScript'){
+            HookPage.cell.styles.fillColor="yellow"
+          }
+          if(HookPage.cell.text=="Python"){
+            HookPage.cell.styles.fillColor="cyan"
+          }
+          console.log(HookPage);
+        }
+      });
+      
+    let count=doc.internal.getNumberOfPages();
+    
+    for(let i=0;i<count;i++){
+      doc.setPage(i);
+      doc.setFontSize(12);
+      doc.line(20, 25, 60, 25);
+      doc.text(`Page ${i+1} of ${count}`,15,doc.internal.pageSize.height-10);
+      // console.log(doc.internal.getCurrentPageInfo());
+    }
+
+      doc.save('yeah.pdf'); 
+    }
+  },
+  created() {
+    this.fetchData();
   }
+}
 </script>
